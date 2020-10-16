e92a3aa42bed5e3dddf181ed9d2f9d93b02eff85
MoYunzheng <moyunzheng@gmail.com>
2020-10-16 19:30:58 +0800

    psbcoa

--- js/psbcoa_ui.js
+++ js/psbcoa_ui.js
@@ -8,11 +8,8 @@ $('head').html('\
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> \
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>\
   	<meta name="viewport" content="width=device-width, initial-scale=1">\
-  	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">\
   	<link rel="stylesheet" href="/resources/demos/style.css">\
-  	<script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js"></script>\
-	<script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>\
-  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>');
+  	<script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js"></script>');
 
 
 $('head').append('\
